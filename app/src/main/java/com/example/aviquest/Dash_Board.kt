ed6a140225dbd0e7ed2e741b16aca5e7620b749e
GrizzlllllyT26 <kamogelomafuna2000@gmail.com>
2023-09-28 19:34:22 +0000

    ADDED THE DASH BOARD

---
+++ app/src/main/java/com/example/aviquest/Dash_Board.kt
@@ -0,0 +1,11 @@
+package com.example.aviquest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Dash_Board : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dash_board)
+    }
+}
