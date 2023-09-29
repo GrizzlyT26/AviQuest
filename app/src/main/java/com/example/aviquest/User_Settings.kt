a8e3154add074dbd09d9f02a9e3f67172076792b
GrizzlllllyT26 <kamogelomafuna2000@gmail.com>
2023-09-29 01:30:47 +0000

    ADDED THE DASH BOARD, Forgot Password and User Settings

---
+++ app/src/main/java/com/example/aviquest/User_Settings.kt
@@ -0,0 +1,11 @@
+package com.example.aviquest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class User_Settings: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_settings)
+    }
+}
